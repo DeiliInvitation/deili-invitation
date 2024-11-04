a39deab21c1e8448314107693c55d085b950b186
N Rizki Amalia <rizkiamel9@gmail.com>
2024-11-04 17:20:23 +0700

    deili invitation initialisation

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,74 @@
 import Image from "next/image";
+import Link from "next/link";
+import { FaWhatsapp, FaInstagram } from "react-icons/fa";
+import deilibg from "@/public/deili-bg.webp";
+import deiliimg from "@/public/deili-img.webp";
+import logo from "@/public/logo.png";
+
+const contact = [
+  {
+    icon: FaWhatsapp,
+    name: "WhatsApp",
+    link: "https://wa.me/6285198910919?text=halo,%20saya%20ingin%20bertanya%20mengenai%20wedding%20invitation%20website",
+  },
+  {
+    icon: FaInstagram,
+    name: "Instagram",
+    link: "https://instagram.com/deiliinvitation",
+  },
+];
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="relative ">
+      <div className="absolute top-0 left-0 md:left-20 lg:left-40 z-30">
+        <div className="p-5 flex items-center gap-10">
+          {contact.map((item, index) => (
+            <Link
+              key={index}
+              href={item.link}
+              className="flex items-center gap-2 text-xl text-white md:text-black "
+            >
+              <item.icon className="text-2xl" /> {item.name}{" "}
+            </Link>
+          ))}
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+      <div className="w-full h-full md:w-screen md:h-screen flex flex-col-reverse md:flex-row md:justify-between relative">
+        <div className="flex w-full h-full md:w-1/2 items-center justify-center bg-yellow-50">
+          <div className="flex flex-col-reverse justify-end h-full md:flex-col gap-5 pt-20 pb-10 md:py-5">
+            <Image
+              src={deiliimg}
+              alt="image"
+              className="w-[300px] h-[400px] object-cover object-top grayscale"
+            />
+            <p className="text-xl">
+              Crafting memories, <br />
+              design your dream, <br />
+              with us.
+            </p>
+          </div>
+        </div>
+        <div className="p-5 absolute top-[45%] left-[35%] md:top-[40%] md:left-[42%] lg:left-[45%] z-50">
+          <Image src={logo} alt="logo" className="w-24 h-24" />
+        </div>
+        <div className="w-full md:w-1/2 relative overflow-hidden">
+          <div className="h-screen w-full flex flex-col items-center justify-center">
+            <div className="absolute z-10 h-screen w-full bg-black opacity-40 md:opacity-20"></div>
+            <div className="absolute top-0 left-0 h-screen w-full">
+              <Image
+                src={deilibg}
+                alt="background"
+                className="h-screen w-full object-cover object-center"
+              />
+            </div>
+            <h1 className="z-20 font-sallim text-8xl font-light text-yellow-50 ">
+              Deili <br />
+              <span className="ml-10">Invitation</span>
+            </h1>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sallim: "var(--font-sallim)"
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
